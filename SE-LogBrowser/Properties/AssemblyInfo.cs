8dcb04771943c9b38f21db65b558874763c0497f
Tom B <me@iaero.me>
2016-05-07 22:05:12 +0100

    .NET 4

--- SE-LogBrowser/Properties/AssemblyInfo.cs
+++ SE-LogBrowser/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SE-LogBrowser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Log Browser for SoftEther VPN Server")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Aero UK")]
 [assembly: AssemblyProduct("SE-LogBrowser")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
